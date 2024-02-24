a9f4ab062e2fcddd912d6575eb31f768eb548792
Mrinal <mrinalkandwal@gmail.com>
2024-02-24 11:55:25 +0530

    fixing bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -94,7 +94,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const { email, username, password } = req.body;
 
   //username or email
-  if (!username || !email) {
+  if (!username && !email) {
     throw new ApiError(400, "username or password is required");
   }
   //find user
